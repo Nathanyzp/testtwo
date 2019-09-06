2c26bfd50d02acdb898710cb3c524b1cf7bf67b0
yuanzhipang <568624829@qq.com>
2019-09-06 15:15:40 +0800

    opoupojp

--- src/git_demo_first/GoodMorning.java
+++ src/git_demo_first/GoodMorning.java
@@ -3,6 +3,7 @@ package git_demo_first;
 public class GoodMorning {
 
 	public static void main(String[] args) {
-		System.out.println("good morning");
+		System.out.println("good morningoooo");
+		
 	}
 }
